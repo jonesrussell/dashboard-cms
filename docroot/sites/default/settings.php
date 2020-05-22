d005fba5f98777a0aab67e152336f128733d87db
Russell Jones <jonesrussell42@gmail.com>
2020-05-22 04:33:46 -0400

    Setup ddev

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -288,3 +288,8 @@ $settings['flysystem'] = $schemes;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+// Automatically generated include for settings managed by ddev.
+if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}
